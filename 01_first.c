e90442d0ab0e3c945d52f1ff426f3f38cb244a92
suraj0522 <surajk256666@gmail.com>
2025-04-18 18:52:32 +0530

    Add files via upload

---
+++ 01_first.c
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+
+// area of square
+int main() {
+   float radius;
+       printf("enter radius");
+       scanf("%f", &radius);
+
+       printf("area is : %f", 3.14 * radius * radius);
+       return 0;
+
+
+}
+
+// printf("Hello World\n"); 
+// int number =20; 
+// int age = 20; 
+// age = 22; 
+// char star = '*'; 
+// int final_price =2350; 
+// float pi = 3.14; 
+// printf("this is a number\n"); 
+// printf("this is a number\n"); 
+// printf("this is a number\n"); 
+// printf("age %d\n", age); 
+   //  int a,b;
+   //  printf("enter a");
+   //   scanf("%d",&a);
+   //    printf("enter b");
+   //     scanf("%d",&b);
+   //  //    int sum = a + b;
+   //     printf("sum is : %d",a + b);
