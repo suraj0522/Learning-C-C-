932310096c24d6b819d7d0c70c67f9da9456db4b
suraj0522 <surajk256666@gmail.com>
2025-04-18 18:52:00 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,73 @@
+# include <stdio.h>
+  # include<math.h>
+
+         int main() {
+        //    int a =22; 
+        //      int b = a;
+        //       int c = b * 2;
+        //        int d = 1 , e;
+        //        printf ("%d", a);
+
+            //    int oldage =25;
+            //    int years =5;
+            //   int newage = oldage +years;
+            //   int a,b,c;
+            //   int a= b =c =2;
+            //   int a = a+b;
+            //   int b = a*b;
+              // int power= pow(a,2);
+              // printf("%d",power);
+
+              // printf("%f/n", 4 / 2.0);
+
+              // int a = 5 + 2 / 2 * 3;
+              // int a = 2;
+              // int b = 5;
+              // a += b;
+              // printf("%d \n", a);
+
+              // printf("%d \n", 5 >= 5 && 4 > 2 );
+              // printf("%d \n", !(4 ==4) );
+
+            // int x;
+            // even =1
+            // odd =0
+
+            // printf("Enter a number :");
+            // scanf("%d", &x);
+            // printf("%d  \n", x % 2 ==0);
+
+            // printf("%d \n", 1);
+
+            // int z; int y =z ; valid statement
+            // char star = "**"; invalid statement
+            int  issunday =0;
+            int isnowing =1;
+            printf("%d \n", issunday && isnowing);
+
+            int ismonday =0;
+            int israining =1;
+            printf("%d \n" , ismonday|| israining);
+
+            // int x;
+            // printf("enter a number:");
+            // scanf("%d", &x);
+            // printf("%d \n", x>9 && x<100);
+
+            int age;
+            printf(" enter age :");
+            scanf("%d", &age);
+            // if ( age >=18){
+            //    printf("adult \n");
+            // }
+            // else if ( age >13 && age <18) {
+            //    printf("teenage \n");
+            // }
+            // else{
+            //    printf(" child \n");
+            // }
+
+            age >= 18 ? printf("adult \n") : printf( "minor \n");
+
+            return 0;
+         }
