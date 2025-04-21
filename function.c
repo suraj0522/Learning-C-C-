9fd70d1302f7f5e30f818d3519ec6d9d16ea3519
suraj0522 <surajk256666@gmail.com>
2025-04-21 20:01:59 +0530

    add namaste &bonjoure

--- function.c
+++ function.c
@@ -1,16 +1,40 @@
 #include <stdio.h>
 
-void printhello();
+// void printhello();
+// void printgdbye();
+void namaste();
+void bonjour();
 int main(){
+       printf("neter f for french & i for india \n");
+       char ch;
+       scanf("%c", &ch);
 
-     printhello();
-     printhello();
-     printhello();
+       if (ch =='i'){
+        namaste();
+       }else{
+          bonjour();
+       }
+       
+       
+     
      
     return 0;
 }
 
-void printhello() {
-     printf( "hello!\n");
-     printf("hey  there \n");
+// void printhello() {
+//      printf( "hello!\n");
+//      printf("hey  there \n");
+// }
+// void printgdbye() {
+//      printf( "goodbye!\n");
+     
+// }
+
+void namaste(){
+     printf("namaste \n");
+
+}
+void bonjour(){
+     printf("bonjour \n");
+
 }
