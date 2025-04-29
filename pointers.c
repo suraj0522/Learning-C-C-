73ba1e3a2b1b3199299ea5d5f91b749bdd894ec8
suraj0522 <surajk256666@gmail.com>
2025-04-29 20:52:52 +0530

    add pointer address

--- pointers.c
+++ pointers.c
@@ -1,9 +1,17 @@
 #include<stdio.h>
 
 int main(){
+    int age =10;
+    int *ptr = &age;
     
-         
+     
+    printf("%p\n",&age);
+    printf("%u\n",&age);
+    printf("%u\n",ptr);
+    printf("%u\n",&ptr);
 
+         
+    
 
 
 
