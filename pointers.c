d5068d8d52025965ff5b21832fae270759af59aa
suraj0522 <surajk256666@gmail.com>
2025-05-01 20:46:19 +0530

    add ptr in new var

--- pointers.c
+++ pointers.c
@@ -2,13 +2,38 @@
 
 int main(){
     int age =10;
-    int *ptr = &age;
+    // int *ptr = &age;
     
      
-    printf("%p\n",&age);
-    printf("%u\n",&age);
-    printf("%u\n",ptr);
-    printf("%u\n",&ptr);
+    // printf("%p\n",&age);
+    // printf("%u\n",&age);
+    // printf("%u\n",ptr);
+    // printf("%u\n",&ptr);
+
+    // printf("%d\n",*ptr);
+    printf("%d\n",age);
+    printf("%d\n",*(&age));
+
+    int x;
+    int *ptr;
+
+    ptr =&x;
+    *ptr = 0;
+
+    printf("x = %d\n", x);
+    printf("x = %d\n", *ptr);
+
+    *ptr +=5;
+    printf("x = %d\n", x);
+    printf("x = %d\n", *ptr);
+
+    (*ptr)++;
+    printf("x = %d\n", x);
+    printf("x = %d\n", *ptr);
+
+
+
+
 
          
     
