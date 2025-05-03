ac9057531ddc61f1118480883101f24a73524c71
suraj0522 <surajk256666@gmail.com>
2025-05-03 20:41:16 +0530

    add swap function

--- pointers.c
+++ pointers.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 
+void square(int n);
+void _square(int* n);
+void swap(int a,int b);
+
+
+
 int main(){
-    int age =10;
+    // int age =10;
     // int *ptr = &age;
     
      
@@ -11,25 +17,50 @@ int main(){
     // printf("%u\n",&ptr);
 
     // printf("%d\n",*ptr);
-    printf("%d\n",age);
-    printf("%d\n",*(&age));
+    // printf("%d\n",age);
+    // printf("%d\n",*(&age));
 
-    int x;
-    int *ptr;
+    // int x;
+    // int *ptr;
 
-    ptr =&x;
-    *ptr = 0;
+    // ptr =&x;
+    // *ptr = 0;
 
-    printf("x = %d\n", x);
-    printf("x = %d\n", *ptr);
+    // printf("x = %d\n", x);
+    // printf("x = %d\n", *ptr);
 
-    *ptr +=5;
-    printf("x = %d\n", x);
-    printf("x = %d\n", *ptr);
+    // *ptr +=5;
+    // printf("x = %d\n", x);
+    // printf("x = %d\n", *ptr);
 
-    (*ptr)++;
-    printf("x = %d\n", x);
-    printf("x = %d\n", *ptr);
+    // (*ptr)++;
+    // printf("x = %d\n", x);
+    // printf("x = %d\n", *ptr);
+
+    // float price =100.00;
+    // float *ptr2 =&price;
+    // float **pptr =&ptr2;
+
+    int i =10;
+    int *ptr2 =&i;
+    int **pptr = &ptr2;
+    printf("%d\n",**pptr);
+
+    int number = 5;
+    square(number);
+    printf("number = %d\n", number);
+
+    _square(&number);
+    printf("number = %d\n", number);
+
+    int x =3 ;
+    int y= 5;
+    swap(x,y);
+    printf("a = %d\n", x);
+    printf("b = %d\n", y);
+
+
+
 
 
 
@@ -42,4 +73,21 @@ int main(){
 
 
     return 0;
+}
+
+void square(int n){
+     n = n * n;
+     printf("sqare = %d \n", n);
+}
+void _square(int* n){
+        *n = *n * *n;
+        printf("sqare = %d \n", *n);
+}
+
+void swap(int a,int b){
+    int t= a;
+    a = b;
+    b = t;
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
 }
