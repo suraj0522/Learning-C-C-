6faaed1372790ac523e705b5373bbe496b11d491
suraj0522 <surajk256666@gmail.com>
2025-05-04 20:06:51 +0530

    using array to enter marks of sub

--- arrays.c
+++ arrays.c
@@ -1,7 +1,24 @@
 #include<stdio.h>
 
 int main() {
-    
+    // int marks []={45, 78, 90, 56, 89};
+    // printf("The marks are:%d\n",marks[4]);
+    int marks[3];
+    printf("enter phy marks :");
+    scanf("%d",&marks[0]);
+
+    printf("enter chem marks :");
+    scanf("%d",&marks[1]);
+
+    printf("enter math marks :");
+    scanf("%d",&marks[2]);
+
+    printf(" phy =%d chem =%d math =%d\n",marks[0],marks[1],marks[2]);
+
+
+
+
+
     
     return 0;
 }
